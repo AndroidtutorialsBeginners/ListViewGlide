60c67b6a57e3edb5c923c573e4b4d59ff4259030
swarathesh <swarathesh60@gmail.com>
2017-09-03 16:37:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/swarathesh60/listviewglide/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.swarathesh60.listviewglide;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+    public static String[] eatFoodyImages = {
+            "http://i.imgur.com/rFLNqWI.jpg",
+            "http://i.imgur.com/C9pBVt7.jpg",
+            "http://i.imgur.com/rT5vXE1.jpg",
+            "http://i.imgur.com/aIy5R2k.jpg",
+            "http://i.imgur.com/MoJs9pT.jpg",
+            "http://i.imgur.com/S963yEM.jpg",
+            "http://i.imgur.com/rLR2cyc.jpg",
+            "http://i.imgur.com/SEPdUIx.jpg",
+            "http://i.imgur.com/aC9OjaM.jpg",
+            "http://i.imgur.com/76Jfv9b.jpg",
+            "http://i.imgur.com/fUX7EIB.jpg",
+            "http://i.imgur.com/syELajx.jpg",
+            "http://i.imgur.com/COzBnru.jpg",
+            "http://i.imgur.com/Z3QjilA.jpg",
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(
+                new ImageListAdapter(
+                       getApplicationContext(),
+                        eatFoodyImages
+                )
+        );
+    }
+    public class ImageListAdapter extends ArrayAdapter {
+        private Context context;
+        private LayoutInflater inflater;
+
+        private String[] imageUrls;
+
+        public ImageListAdapter(Context context, String[] imageUrls) {
+            super(context, R.layout.listview_item_image, imageUrls);
+
+            this.context = context;
+            this.imageUrls = imageUrls;
+
+            inflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (null == convertView) {
+                convertView = inflater.inflate(R.layout.listview_item_image, parent, false);
+            }
+
+            Glide
+                    .with(context)
+                    .load(imageUrls[position])
+                    .into((ImageView) convertView);
+
+            return convertView;
+        }
+    }
+}
